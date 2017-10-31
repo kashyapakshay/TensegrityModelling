5b27db34852e025dffa43b4fcc7c43e9670e84a1
Akshay Kashyap <kashyapa@union.edu>
2017-10-31 00:23:01 -0400

    Test Springs

--- tensegrity/components/testclass.cpp
+++ tensegrity/components/testclass.cpp
@@ -2,15 +2,18 @@
 
 #include "Motor.h"
 #include "Strut.h"
+#include "Spring.h"
 
 using namespace std;
 
 int main() {
     Motor motor(1, 0.695);
-    Strut strut;
+    Strut strut, strut2;
+    Spring spring(strut, 1, strut2, -1);
 
     cout << motor.get_frequency() << "\n";
     cout << strut.get_mass() << "\n";
+    cout << spring.compute_spring_force() << "\n";
 
     return 0;
 }
