91973a8fdaaf963ed06cb44554ece34ac06549c2
Akshay Kashyap <kashyapa@union.edu>
2018-01-21 14:35:51 -0500

    Finished basic abstractions for a Strut

--- tensegrity/components/dynamic/Strut.cpp
+++ tensegrity/components/dynamic/Strut.cpp
@@ -4,9 +4,11 @@ int Strut::TOP_EDGE = 1;
 int Strut::BOTTOM_EDGE = -1;
 
 Strut::Strut(dWorldID world, dSpaceID space) {
-	_coords = {0, 0, 0};
+	_length = 1.0;
+
+	_coords = {0, 0, _length};
 	_angles = {0, 0, 0};
-	_length = 0.1;
+
 	_mass = 1.0;
 	_radius = 0.02;
 	_color = {0, 0, 0};
@@ -17,85 +19,96 @@ Strut::Strut(dWorldID world, dSpaceID space) {
 	_space = space;
 	_body = dBodyCreate(world);
 
-	_geom = dCreateCapsule(space, _radius, _length);
+	_geom = dCreateCapsule(_space, _radius, _length);
 
 	dMassSetZero(&_mass_obj);
     dMassSetCapsule(&_mass_obj, _DENSITY, 3, _radius, _length);
     dBodySetMass(_body, &_mass_obj);
 
-    dBodySetPosition(_body, _coords[0], _coords[1], _coords[2]);
-    dBodySetRotation(_body, _angles);
+	dMatrix3 _rot;
+	dRFromEulerAngles(_rot, _angles[0], _angles[1], _angles[2]);
+
+    dBodySetPosition(_body, (float)_coords[0], (float)_coords[1], (float)_coords[2]);
+    dBodySetRotation(_body, _rot);
 
     dGeomSetBody(_geom, _body);
 }
 
-Strut::Strut(dWorldID world, dSpaceID space, d_vector coords, d_vector angles) {
-	_coords = coords;
-	_angles = angles;
-	_length = 0.1;
-	_mass = 1.0;
-	_radius = 0.02;
-	_color = {0, 0, 0};
+// Strut::Strut(dWorldID world, dSpaceID space, d_vector coords, d_vector angles) {
+// 	_coords = coords;
+// 	_angles = angles;
+// 	_length = 0.1;
+// 	_mass = 1.0;
+// 	_radius = 0.02;
+// 	_color = {0, 0, 0};
+//
+// 	// _motor = NULL;
+//
+// 	_world = world;
+// 	_space = space;
+// 	_body = dBodyCreate(world);
+//
+// 	_geom = dCreateCapsule(space, _radius, _length);
+//
+// 	dMassSetZero(&_mass_obj);
+//     dMassSetCapsule(&_mass_obj, _DENSITY, 3, _radius, _length);
+//     dBodySetMass(_body, &_mass_obj);
+//
+// 	dMatrix3 _rot;
+// 	dRFromEulerAngles(_rot, _angles[0], _angles[1], _angles[2]);
+//
+//     dBodySetPosition(_body, _coords[0], _coords[1], _coords[2]);
+//     dBodySetRotation(_body, _rot);
+//
+//     dGeomSetBody(_geom, _body);
+// }
+//
+// // dSpaceID space, dWorldID world
+// Strut::Strut(dWorldID world, dSpaceID space, d_vector coords,
+// 	d_vector angles, double mass, double length, double radius, d_vector color) {
+//
+// 	_coords = coords;
+// 	_angles = angles;
+// 	_length = length;
+// 	_mass = mass;
+// 	_radius = radius;
+// 	_color = color;
+//
+// 	// _motor = NULL;
+//
+// 	_world = world;
+// 	_space = space;
+// 	_body = dBodyCreate(world);
+//
+// 	_geom = dCreateCapsule(space, _radius, _length);
+//
+// 	dMassSetZero(&_mass_obj);
+//     dMassSetCapsule(&_mass_obj, _DENSITY, 3, _radius, _length);
+//     dBodySetMass(_body, &_mass_obj);
+//
+// 	dMatrix3 _rot;
+// 	dRFromEulerAngles(_rot, _angles[0], _angles[1], _angles[2]);
+//
+//     dBodySetPosition(_body, _coords[0], _coords[1], _coords[2]);
+//     dBodySetRotation(_body, _rot);
+//
+//     dGeomSetBody(_geom, _body);
+// }
 
-	// _motor = NULL;
+dBodyID Strut::get_body() {return _body;}
 
-	_world = world;
-	_space = space;
-	_body = dBodyCreate(world);
-
-	_geom = dCreateCapsule(space, _radius, _length);
-
-	dMassSetZero(&_mass_obj);
-    dMassSetCapsule(&_mass_obj, _DENSITY, 3, _radius, _length);
-    dBodySetMass(_body, &_mass_obj);
-
-    dBodySetPosition(_body, _coords[0], _coords[1], _coords[2]);
-    dBodySetRotation(_body, _angles);
-
-    dGeomSetBody(_geom, _body);
-}
-
-// dSpaceID space, dWorldID world
-Strut::Strut(dWorldID world, dSpaceID space, d_vector coords,
-	d_vector angles, double mass, double length, double radius, d_vector color) {
-
-	_coords = coords;
-	_angles = angles;
-	_length = length;
-	_mass = mass;
-	_radius = radius;
-	_color = color;
-
-	// _motor = NULL;
-
-	_world = world;
-	_space = space;
-	_body = dBodyCreate(world);
-
-	_geom = dCreateCapsule(space, _radius, _length);
-
-	dMassSetZero(&_mass_obj);
-    dMassSetCapsule(&_mass_obj, _DENSITY, 3, _radius, _length);
-    dBodySetMass(_body, &_mass_obj);
-
-    dBodySetPosition(_body, _coords[0], _coords[1], _coords[2]);
-    dBodySetRotation(_body, _angles);
-
-    dGeomSetBody(_geom, _body);
-}
-
-double Strut::get_mass() {return _mass;}
-double Strut::get_radius() {return _radius;}
-double Strut::get_length() {return _length;}
+dReal Strut::get_mass() {return _mass;}
+dReal Strut::get_radius() {return _radius;}
+dReal Strut::get_length() {return _length;}
 
 d_vector Strut::get_coords() {return _coords;}
 d_vector Strut::get_color() {return _color;}
-d_vector Strut::get_angles() {
-	dMatrix3 angle;
-	angle =	dBodyGetRotation(_body);
-
-	return {angle[0], angle[1], angle[2]};
-}
+// d_vector Strut::get_angles() {
+// 	dMatrix3 angle;
+// 	angle =	dBodyGetRotation(_body);
+//
+// 	return {angle[0], angle[1], angle[2]};
+// }
 
 d_vector Strut::get_edge_coords(int edge_dir) {
 	dVector3 edge_coords;
@@ -114,6 +127,11 @@ void Strut::apply_edge_force(int edge_dir, d_vector forces) {
         0, 0, edge_dir * _length / 2
 	);
 }
+
+// void Strut::draw() {
+// 	dsDrawCapsule(dBodyGetPosition(get_body()),
+//         dBodyGetRotation(get_body()), get_length(), get_radius());
+// }
 
 // void Strut::attach_motor(Motor *motor) {_motor = motor;}
 // Motor* Strut::get_attached_motor() {return _motor;}
--- tensegrity/components/dynamic/Strut.h
+++ tensegrity/components/dynamic/Strut.h
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include "ode/ode.h"
+#include "drawstuff/drawstuff.h"
 
 // #include "Motor.h"
 
@@ -12,24 +13,27 @@ typedef std::vector<double> d_vector;
 class Strut {
     public:
         Strut(dWorldID, dSpaceID);
-        Strut(dWorldID, dSpaceID, d_vector, d_vector);
-        Strut(dWorldID, dSpaceID, d_vector, d_vector, double, double, double, d_vector);
+        // Strut(dWorldID, dSpaceID, d_vector, d_vector);
+        // Strut(dWorldID, dSpaceID, d_vector, d_vector, double, double, double, d_vector);
 
         static int TOP_EDGE, BOTTOM_EDGE;
 
-		double get_mass();
-		double get_length();
-		double get_radius();
+        dBodyID get_body();
+
+		dReal get_mass();
+		dReal get_length();
+		dReal get_radius();
 
 		d_vector get_coords();
         d_vector get_color();
-		d_vector get_angles();
+		// d_vector get_angles();
 
 		d_vector get_edge_coords(int);
         d_vector get_top_edge_coords();
         d_vector get_bottom_edge_coords();
 
 		void apply_edge_force(int, d_vector);
+        // void draw();
 
         // void attach_motor(Motor*);
         // Motor* get_attached_motor();
@@ -43,11 +47,11 @@ class Strut {
         d_vector _coords;
         d_vector _angles;
         d_vector _color;
-        double _length, _mass, _radius;
+        dReal _length, _mass, _radius;
 
         dMass _mass_obj;
 
-        const float _DENSITY = 0.5;
+        const double _DENSITY = 0.5;
 
         // Motor *_motor;
 };
